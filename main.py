96fca658f05bac911bce4152cb7f789e6522c7b2
Dmitrii Okhlopkov <91318266+tmokkuss@users.noreply.github.com>
2022-11-17 16:23:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,56 +1,55 @@
-def read_file(file_name):
-    f = open(file_name)
-    content = f.read().splitlines()
-    return content
+import unittest
+import convert
 
 
-def format_key_value(key, value):
-    return 4 * ' ' + '"' + key + '"' + ': ' + value
+class TestConvert(unittest.TestCase):
+    def test_format_key_value(self):
+        data = '    "id": 1.0'
+        result = convert.format_key_value('id', '1.0')
+        self.assertEqual(result, data)
+
+    def test_format_value_type_str(self):
+        data = '"Alex"'
+        result = convert.format_value_type('Alex')
+        self.assertEqual(result, data)
+
+    def test_format_value_type_bool(self):
+        data = 'true'
+        result = convert.format_value_type('True')
+        self.assertEqual(result, data)
+
+    def test_format_value_type_number(self):
+        data = '1.0'
+        result = convert.format_value_type('1.0')
+        self.assertEqual(result, data)
+
+    def test_csv_content_to_json_content(self):
+        data = [{"id": 1.0, "name": "Alex", "salary": 150000, "department": True},
+                {"id": 2.5, "name": "Joe", "salary": 200000, "department": False},
+                {"id": 3.2, "name": "Frodo", "salary": 100000, "department": False}]
+        content = convert.read_file('data.csv')
+        result = [
+                  {
+                    "id": 1.0,
+                    "name": "Alex",
+                    "salary": 150000,
+                    "department": True
+                  },
+                  {
+                    "id": 2.5,
+                    "name": "Joe",
+                    "salary": 200000,
+                    "department": False
+                  },
+                  {
+                    "id": 3.2,
+                    "name": "Frodo",
+                    "salary": 100000,
+                    "department": False
+                  }
+                ]
+        self.assertEqual(result, data)
 
 
-def format_value_type(value):
-    if value.isalpha():
-        if value not in ['True', 'False']:
-            return '"' + value + '"'
-        return value.lower()
-    else:
-        return value
-
-
-def write_data(file_name, content):
-    f = open(file_name, "w")
-    f.write(content)
-    f.close()
-
-
-def csv_content_to_json_content(csv_content):
-    keys = csv_content[0].split(',')
-    result = '[' + '\n'
-    csv_content.pop(0)
-    for row_index, row in enumerate(csv_content):
-        result += 2 * ' ' + '{' + '\n'
-        values = row.split(',')
-        zipped_key_value = dict(zip(keys, values))
-        len_dict = len(zipped_key_value)
-
-        for key_index, key in enumerate(zipped_key_value):
-            result += format_key_value(key, format_value_type(zipped_key_value[key]))
-            if key_index + 1 != len_dict:
-                result += ','
-            result += '\n'
-
-        result += 2 * ' ' + '}'
-        if row_index + 1 != csv_content:
-            result += ','
-        result += '\n'
-    result += ']'
-    return result
-
-
-def csv_file_to_json_file(input_file, output_file):
-    csv_content = read_file(input_file)
-    json_result = csv_content_to_json_content(csv_content)
-    write_data(output_file, json_result)
-
-
-csv_file_to_json_file('data.csv', 'data.json')
+if __name__ == '__main__':
+    unittest.main()
